7d66c89ace820476e3754d8fb8bd680873fcd258
hystake <60761436+hystake@users.noreply.github.com>
2020-06-23 11:12:21 -0400

    Update upload_example.py

--- upload_example.py
+++ upload_example.py
@@ -1,40 +1,30 @@
-
 import requests
 import os
 import csv
 import json
 
 '''
-For more details: https://hystakenewapi.docs.apiary.io/
-
+For more details: https://hystakeapialpha.docs.apiary.io/#
 A download query has 3 components:
-1. required: API token
-2. required: user id code
-3. required: product code
-4. required: table code
+1. required: API key
+2. required: dataset code
 '''
 
-# Find / regenerate your API token in Account Info -> Developer Info
-token = "Enter your API token here"
+# To find / regenerate your API token in Account Info -> Developer Info
+apikey = "Enter your API token here"
 
-# the full dataset ID is composed of 3 parts: user ID, product ID, table ID
-seller_id = "Enter your seller ID here"
-product_id = "Enter your product ID here"
-table_id = "Enter your table ID here"
-datasetCode = seller_id + "-" + product_id + "-" + table_id
+# To find dataset ID, go to the product page and copy the dataset ID
+dataset_id = "Enter the dataset ID here"
 
 file_path = "Enter your .csv file path"
-url = 'http://dev.hystake.com/api/v1/dataset/' + datasetCode + '/'
+url = 'http://dev.hystake.com/api/v1/dataset/' + dataset_id + '/'
 mode = 'append'  # append / overwrite
 skiprows = 1  # skip the first n rows of the csv
-url_params = {'token': token, 'mode': mode, 'skiprows': skiprows}
+url_params = {'key': apikey, 'mode': mode, 'skiprows': skiprows}
 
 # upload
 file = open(file_path)
 csv_reader = csv.reader(file)
-request_data = {
+request_data = {'dataset': list(csv_reader)}
 
-    'dataset': list(csv_reader)
-
-}
 r = requests.post(url, params=url_params, json=request_data)
